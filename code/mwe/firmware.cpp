32772493742affd854a68df33643cb5235a61e14
Michael Allwright <allsey87@gmail.com>
2017-03-14 18:17:37 +1100

    Added example code

---
+++ code/mwe/firmware.cpp
@@ -0,0 +1,30 @@
+#include "firmware.h"
+
+/* initialisation of the static singleton */
+Firmware Firmware::_firmware;
+
+/* main function that runs the firmware */
+int main(void)
+{
+   /* FILE structs for fprintf */
+   FILE huart;
+
+   /* Set up FILE structs for fprintf */                           
+   fdev_setup_stream(&huart, 
+                     [](char c_to_write, FILE* pf_stream) {
+                        Firmware::GetInstance().GetHUARTController().Write(c_to_write);
+                        return 1;
+                     },
+                     [](FILE* pf_stream) {
+                        return int(Firmware::GetInstance().GetHUARTController().Read());
+                     },
+                     _FDEV_SETUP_RW);
+
+   Firmware::GetInstance().SetFilePointer(&huart);
+
+   /* Execute the firmware */
+   return Firmware::GetInstance().Exec();
+}
+
+/***********************************************************/
+/***********************************************************/
